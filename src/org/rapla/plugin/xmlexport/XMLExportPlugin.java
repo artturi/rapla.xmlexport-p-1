b1b519caade693dc11101335bd50b55f412deda4
christopher.kohlhaas@gmail.com <christopher.kohlhaas@gmail.com@f78d5ace-84b9-81af-d60f-742a6fa3a8cb>
2013-03-19 19:21:57 +0000

    Removed dependency from deprecated avalon framework. Now the rapla core only depends on java. First preperations for gwt.
    git-svn-id: https://rapla.googlecode.com/svn/plugins/xmlexport_plugin/trunk@3616 f78d5ace-84b9-81af-d60f-742a6fa3a8cb

--- src/org/rapla/plugin/xmlexport/XMLExportPlugin.java
+++ src/org/rapla/plugin/xmlexport/XMLExportPlugin.java
@@ -11,13 +11,13 @@
  | Definition as published by the Open Source Initiative (OSI).             |
  *--------------------------------------------------------------------------*/
 package org.rapla.plugin.xmlexport;
-import org.apache.avalon.framework.configuration.Configuration;
-import org.apache.avalon.framework.logger.Logger;
 import org.rapla.components.xmlbundle.I18nBundle;
 import org.rapla.components.xmlbundle.impl.I18nBundleImpl;
+import org.rapla.framework.Configuration;
 import org.rapla.framework.Container;
 import org.rapla.framework.PluginDescriptor;
 import org.rapla.framework.StartupEnvironment;
+import org.rapla.framework.logger.Logger;
 import org.rapla.plugin.RaplaExtensionPoints;
 import org.rapla.servletpages.DefaultHTMLMenuEntry;
 import org.rapla.servletpages.HTMLMenuExtensionPoint;
